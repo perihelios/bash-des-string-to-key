fe472a3831e7f7aca02d810de0d0dcdc36ae42fd
Jonathan Cottrill <github@jonathancottrill.net>
2020-01-23 18:36:07 -0600

    Add link to Github repo

--- des-string-to-key.sh
+++ des-string-to-key.sh
@@ -25,6 +25,9 @@
 #   this file except in compliance with the License.  You can obtain a copy
 #   in the file LICENSE in the source distribution or at
 #   https://www.openssl.org/source/license.html
+#
+####
+#   Obtainable from: https://github.com/perihelios/bash-des-string-to-key
 ####
 
 ## Recommended Bash options (for any Bash script, really):
